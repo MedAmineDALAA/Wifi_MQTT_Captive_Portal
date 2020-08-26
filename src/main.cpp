63f2420ca42e2e6c1a82e4721078ee9cc4c6e5eb
Mohammed Amine Dalaa <ma.dalaa@NEXTRONIC.local>
2020-08-26 13:25:14 +0200

    MQTT & WIFI Captive Portal

---
+++ src/main.cpp
@@ -0,0 +1,435 @@
+/****************************************************************************************************************************
+ AutoConnect to network from SSID and PASSWORD
+*******************************************************************************************************************************/
+
+#if !( defined(ESP8266) ||  defined(ESP32) )
+  #error This code is intended to run on the ESP8266 or ESP32 platform! Please check your Tools->Board setting.
+#endif
+
+// Use from 0 to 4. Higher number, more debugging messages and memory usage.
+#define _WIFIMGR_LOGLEVEL_    3
+
+//Ported to ESP32
+#ifdef ESP32
+  #include "SPIFFS.h"
+  #include <esp_wifi.h>
+  #include <WiFi.h>
+  #include <WiFiClient.h>
+  
+  #define ESP_getChipId()   ((uint32_t)ESP.getEfuseMac())
+  
+  #define LED_ON      HIGH
+  #define LED_OFF     LOW
+  #define FileFS      SPIFFS
+
+#else
+  #include <ESP8266WiFi.h>          
+  //needed for library
+  #include <DNSServer.h>
+  #include <ESP8266WebServer.h>
+  
+  #define ESP_getChipId()   (ESP.getChipId())
+  
+  #define LED_ON      LOW
+  #define LED_OFF     HIGH
+#endif
+
+#include <ArduinoJson.h>
+
+// SSID and PW for your Router
+String Router_SSID;
+String Router_Pass;
+
+char configFileName[] = "/config.json";
+
+//SSID & PASS for AP 
+String AP_SSID;
+String AP_PASS;
+
+// Use false if you don't like to display Available Pages in Information Page of Config Portal
+// Comment out or use true to display Available Pages in Information Page of Config Portal
+// Must be placed before #include <ESP_WiFiManager.h>
+#define USE_AVAILABLE_PAGES     false
+
+// From v1.0.10 to permit disable/enable StaticIP configuration in Config Portal from sketch. Valid only if DHCP is used.
+// You'll loose the feature of dynamically changing from DHCP to static IP, or vice versa
+// You have to explicitly specify false to disable the feature.
+//#define USE_STATIC_IP_CONFIG_IN_CP          false
+
+// Use false to disable NTP config. Advisable when using Cellphone, Tablet to access Config Portal.
+#define USE_ESP_WIFIMANAGER_NTP     false
+
+// Use true to enable CloudFlare NTP service. System can hang if you don't have Internet access while accessing CloudFlare
+#define USE_CLOUDFLARE_NTP          false
+
+// New in v1.0.11
+#define USING_CORS_FEATURE          true
+//////
+
+// Use USE_DHCP_IP == true for dynamic DHCP IP, false to use static IP which you have to change accordingly to your network
+#if (defined(USE_STATIC_IP_CONFIG_IN_CP) && !USE_STATIC_IP_CONFIG_IN_CP)
+  // Force DHCP to be true
+  #if defined(USE_DHCP_IP)
+    #undef USE_DHCP_IP
+  #endif
+  #define USE_DHCP_IP     true
+#else
+  // You can select DHCP or Static IP here
+  #define USE_DHCP_IP     true
+  //#define USE_DHCP_IP     false
+#endif
+
+#if ( USE_DHCP_IP || ( defined(USE_STATIC_IP_CONFIG_IN_CP) && !USE_STATIC_IP_CONFIG_IN_CP ) )
+// Use DHCP
+  #warning Using DHCP IP
+  IPAddress stationIP   = IPAddress(192, 168, 1, 220);//(0,0,0,0)
+  IPAddress gatewayIP   = IPAddress(192, 168, 1, 1);
+  IPAddress netMask     = IPAddress(255, 255, 255, 0);
+#else
+  // Use static IP
+  #warning Using static IP
+  
+  #ifdef ESP32
+    IPAddress stationIP   = IPAddress(192, 168, 1, 140);
+  #else
+    IPAddress stationIP   = IPAddress(192, 168, 2, 186);
+  #endif
+  
+  IPAddress gatewayIP   = IPAddress(192, 168, 1, 1);
+  IPAddress netMask     = IPAddress(255, 255, 255, 0);
+#endif
+
+#define USE_CONFIGURABLE_DNS      true
+
+IPAddress dns1IP      = gatewayIP;
+IPAddress dns2IP      = IPAddress(8, 8, 8, 8);
+
+#include <ESP_WiFiManager.h>              
+
+//define your default values here, if there are different values in configFileName (config.json), they are overwritten.
+#define BLYNK_SERVER_LEN                64
+#define BLYNK_TOKEN_LEN                 32
+#define BLYNK_SERVER_PORT_LEN           6
+
+#define MQTT_SERVER_MAX_LEN             40
+#define MQTT_SERVER_PORT_LEN            6
+
+char blynk_server [BLYNK_SERVER_LEN]        = "192.168.1.188";
+char blynk_port   [BLYNK_SERVER_PORT_LEN]   = "1880";                  // Broker PORT
+char blynk_token  [BLYNK_TOKEN_LEN]         = "TOKEN";
+
+char mqtt_server  [MQTT_SERVER_MAX_LEN];
+char mqtt_port    [MQTT_SERVER_PORT_LEN]    = "1880";
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback(void)
+{
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+/****************************Functions to check********************************/
+bool loadFileFSConfigFile(void)
+{
+  //clean FS, for testing
+  //FileFS.format();
+
+  //read configuration from FS json
+  Serial.println("Mounting FS...");
+
+  if (FileFS.begin())
+  {
+    Serial.println("Mounted file system");
+
+    if (FileFS.exists(configFileName))
+    {
+      //file exists, reading and loading
+      Serial.println("Reading config file");
+      File configFile = FileFS.open(configFileName, "r");
+
+      if (configFile)
+      {
+        Serial.print("Opened config file, size = ");
+        size_t configFileSize = configFile.size();
+        Serial.println(configFileSize);
+
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[configFileSize + 1]);
+
+        configFile.readBytes(buf.get(), configFileSize);
+
+        Serial.print("\nJSON parseObject() result : ");
+
+  #if (ARDUINOJSON_VERSION_MAJOR >= 6)
+        DynamicJsonDocument json(1024);
+        auto deserializeError = deserializeJson(json, buf.get(), configFileSize);
+
+        if ( deserializeError )
+        {
+          Serial.println("failed");
+          return false;
+        }
+        else
+        {
+          Serial.println("OK");
+
+          if (json["blynk_server"])
+            strncpy(blynk_server, json["blynk_server"], sizeof(blynk_server));
+         
+          if (json["blynk_port"])
+            strncpy(blynk_port, json["blynk_port"], sizeof(blynk_port));
+ 
+          if (json["blynk_token"])
+            strncpy(blynk_token,  json["blynk_token"], sizeof(blynk_token));
+
+          if (json["mqtt_server"])
+            strncpy(mqtt_server, json["mqtt_server"], sizeof(mqtt_server));
+
+          if (json["mqtt_port"])  
+            strncpy(mqtt_port,   json["mqtt_port"], sizeof(mqtt_port));
+        }
+
+        //serializeJson(json, Serial);
+        serializeJsonPretty(json, Serial);
+  #else
+        DynamicJsonBuffer jsonBuffer;
+        // Parse JSON string
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        // Test if parsing succeeds.
+
+        if (json.success())
+        {
+          Serial.println("OK");
+
+          if (json["blynk_server"])
+            strncpy(blynk_server, json["blynk_server"], sizeof(blynk_server));
+         
+          if (json["blynk_port"])
+            strncpy(blynk_port, json["blynk_port"], sizeof(blynk_port));
+ 
+          if (json["blynk_token"])
+            strncpy(blynk_token,  json["blynk_token"], sizeof(blynk_token));
+
+          if (json["mqtt_server"])
+            strncpy(mqtt_server, json["mqtt_server"], sizeof(mqtt_server));
+
+          if (json["mqtt_port"])  
+            strncpy(mqtt_port,   json["mqtt_port"], sizeof(mqtt_port));
+        }
+        else
+        {
+          Serial.println("failed");
+          return false;
+        }
+        //json.printTo(Serial);
+        json.prettyPrintTo(Serial);
+  #endif
+
+        configFile.close();
+      }
+    }
+  }
+  else
+  {
+    Serial.println("failed to mount FS");
+    return false;
+  }
+  return true;
+}
+
+bool saveFileFSConfigFile(void)
+{
+  Serial.println("Saving config");
+
+  #if (ARDUINOJSON_VERSION_MAJOR >= 6)
+  DynamicJsonDocument json(1024);
+  #else
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  #endif
+
+  json["blynk_server"] = blynk_server;
+  json["blynk_port"]   = blynk_port;
+  json["blynk_token"]  = blynk_token;
+
+  json["mqtt_server"] = mqtt_server;
+  json["mqtt_port"]   = mqtt_port;
+
+  File configFile = FileFS.open(configFileName, "w");
+
+  if (!configFile)
+  {
+    Serial.println("Failed to open config file for writing");
+  }
+
+  #if (ARDUINOJSON_VERSION_MAJOR >= 6)
+  //serializeJson(json, Serial);
+  serializeJsonPretty(json, Serial);
+  // Write data to file and close it
+  serializeJson(json, configFile);
+  #else
+  //json.printTo(Serial);
+  json.prettyPrintTo(Serial);
+  // Write data to file and close it
+  json.printTo(configFile);
+  #endif
+
+  configFile.close();
+  //end save
+}
+
+void heartBeatPrint(void)
+{
+  static int num = 1;
+
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.print("H");        // H means connected to WiFi
+  else
+    Serial.print("F");        // F means not connected to WiFi
+
+  if (num == 80)
+  {
+    Serial.println();
+    num = 1;
+  }
+  else if (num++ % 10 == 0)
+  {
+    Serial.print(" ");
+  }
+}
+
+/**************************************************************************/
+void check_status()
+{
+  static ulong checkstatus_timeout = 0;
+
+#define HEARTBEAT_INTERVAL    10000L
+  // Print hearbeat every HEARTBEAT_INTERVAL (10) seconds.
+  if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
+  {
+    heartBeatPrint();
+    checkstatus_timeout = millis() + HEARTBEAT_INTERVAL;
+  }
+}
+
+/**************************************************************************/
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+  
+  Serial.println("\nStarting AutoConnectAP on " + String(ARDUINO_BOARD));
+  loadFileFSConfigFile();
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  ESP_WMParameter custom_blynk_server("blynk_server", "blynk_server", blynk_server, BLYNK_SERVER_LEN + 1);
+  ESP_WMParameter custom_blynk_port  ("blynk_port",   "blynk_port",   blynk_port,   BLYNK_SERVER_PORT_LEN + 1);
+  ESP_WMParameter custom_blynk_token ("blynk_token",  "blynk_token",  blynk_token,  BLYNK_TOKEN_LEN + 1 );
+
+  ESP_WMParameter custom_mqtt_server("mqtt_server", "mqtt_server", mqtt_server, MQTT_SERVER_MAX_LEN + 1);
+  ESP_WMParameter custom_mqtt_port  ("mqtt_port",   "mqtt_port",   mqtt_port,   MQTT_SERVER_PORT_LEN + 1);
+
+  // Use this to default DHCP hostname to ESP8266-XXXXXX or ESP32-XXXXXX
+  //ESP_WiFiManager ESP_wifiManager;
+  // Use this to personalize DHCP hostname (RFC952 conformed)
+  ESP_WiFiManager ESP_wifiManager("AutoConnect-FSParams");
+  
+  //set config save notify callback
+  ESP_wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //add all parameters here
+  ESP_wifiManager.addParameter(&custom_blynk_server);
+  ESP_wifiManager.addParameter(&custom_blynk_port);
+  ESP_wifiManager.addParameter(&custom_blynk_token);
+
+  ESP_wifiManager.addParameter(&custom_mqtt_server);
+  ESP_wifiManager.addParameter(&custom_mqtt_port);
+
+  ESP_wifiManager.setDebugOutput(true);
+
+  //set custom ip for portal
+  ESP_wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+
+  ESP_wifiManager.setMinimumSignalQuality(-1);
+
+  // From v1.0.10 only
+  // Set config portal channel, default = 1. Use 0 => random channel from 1-13
+  ESP_wifiManager.setConfigPortalChannel(0);
+  //////
+
+  #if !USE_DHCP_IP    
+  #if USE_CONFIGURABLE_DNS  
+    // Set static IP, Gateway, Subnetmask, DNS1 and DNS2. New in v1.0.5
+    ESP_wifiManager.setSTAStaticIPConfig(stationIP, gatewayIP, netMask, dns1IP, dns2IP);  
+  #else
+    // Set static IP, Gateway, Subnetmask, Use auto DNS1 and DNS2.
+    ESP_wifiManager.setSTAStaticIPConfig(stationIP, gatewayIP, netMask);
+  #endif 
+  #endif  
+
+  // SSID and Password stored in ESP32 wifi_ap_record_t and wifi_config_t are also cleared in reboot
+  // Have to create a new function to store in EEPROM/SPIFFS for this purpose
+  Router_SSID = ESP_wifiManager.WiFi_SSID();
+  Router_Pass = ESP_wifiManager.WiFi_Pass();
+
+  //Remove this line if you do not want to see WiFi password printed
+  Serial.println("Stored: SSID = " + Router_SSID + ", Pass = " + Router_Pass);
+
+  if (Router_SSID != "")
+  {
+    ESP_wifiManager.setConfigPortalTimeout(60); //If no access point name has been previously entered disable timeout.
+    Serial.println("Got stored Credentials. Timeout 60s");
+  }
+  else
+  {
+    Serial.println("No stored Credentials. No timeout");
+  }
+
+  String chipID = String(ESP_getChipId(), HEX);
+  chipID.toUpperCase();
+
+  // SSID and PW for Config Portal AP_SSID & AP_Password
+  String AP_SSID = "ESP_" + chipID + "_AutoConnectAP";
+  String AP_PASS = "MyESP_" + chipID;
+
+  // Get Router SSID and PASS from EEPROM, then open Config portal AP named "ESP_XXXXXX_AutoConnectAP" and PW "MyESP_XXXXXX"
+  // 1) If got stored Credentials, Config portal timeout is 60s
+  // 2) If no stored Credentials, stay in Config portal until get WiFi Credentials
+  ESP_wifiManager.autoConnect(AP_SSID.c_str(), AP_PASS.c_str());
+  if(!ESP_wifiManager.autoConnect(AP_SSID.c_str(), AP_PASS.c_str()))
+  {
+    ESP.restart();
+    Serial.println("ESP restarting");
+    delay(1000);
+  }
+  //or use this for Config portal AP named "ESP_XXXXXX" and no password (public AP)
+  //==> ESP_wifiManager.autoConnect();
+
+  //if you get here you have connected to the WiFi
+  Serial.println("WiFi connected");
+  
+  //read updated parameters
+  strncpy(blynk_server, custom_blynk_server.getValue(),sizeof(blynk_server));
+  strncpy(blynk_port, custom_blynk_port.getValue(),sizeof(blynk_port));
+  strncpy(blynk_token, custom_blynk_token.getValue(),sizeof(blynk_token));
+  strncpy(mqtt_server, custom_mqtt_server.getValue(),sizeof(mqtt_server));
+  strncpy(mqtt_port, custom_mqtt_port.getValue(),sizeof(mqtt_port));
+
+  if(shouldSaveConfig)
+  {
+    saveFileFSConfigFile();
+  }
+
+  Serial.print("LocalIP= ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  check_status(); // H means connected //F means not connected
+}
